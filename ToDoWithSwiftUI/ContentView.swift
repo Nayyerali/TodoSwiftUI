49c0cd37ba72253b245526c771a450be7db5e449
Nayyerali <nayyer.ali@siliconplex.com>
2020-11-05 10:15:07 +0500

    Initial Commit

---
+++ ToDoWithSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ToDoWithSwiftUI
+//
+//  Created by Siliconplex on 05/11/2020.
+//  Copyright © 2020 Siliconplex. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
